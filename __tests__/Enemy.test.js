bea3a0c0fce5638a3aee05fccf1496b292839081
Curtis Smith <cj1988333@hotmail.com>
2021-02-12 23:47:11 -0500

    finished game logic

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,31 +14,46 @@ test('creates an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 });
-Enemy.prototype.getHealth = function() {
-    return `The ${this.name}'s health is now ${this.health}!`;
-  };
+test('gets enemys health value', () => {
+    const enemy = new Enemy('goblin', 'sword');
   
-  Enemy.prototype.isAlive = function() {
-    if (this.health === 0) {
-      return false;
-    }
-    return true;
-  };
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+  });
   
-  Enemy.prototype.getAttackValue = function() {
-    const min = this.strength - 5;
-    const max = this.strength + 5;
+  test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
   
-    return Math.floor(Math.random() * (max - min) + min);
-  };
+    expect(enemy.isAlive()).toBeTruthy();
   
-  Enemy.prototype.reduceHealth = function(health) {
-    this.health -= health;
+    enemy.health = 0;
   
-    if (this.health < 0) {
-      this.health = 0;
-    }
-  };
-  Enemy.prototype.getDescription = function() {
-    return `A ${this.name} holding a ${this.weapon} has appeared!`;
-  };
+    expect(enemy.isAlive()).toBeFalsy();
+  });
+  
+  test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+  
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+  
+  test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+  
+    enemy.reduceHealth(5);
+  
+    expect(enemy.health).toBe(oldHealth - 5);
+  
+    enemy.reduceHealth(99999);
+  
+    expect(enemy.health).toBe(0);
+  });
+
+  test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+  });
